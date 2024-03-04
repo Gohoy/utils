db979d0e6048eb5849e7bf8fb6aa9214338e7af6
高泓宇 <2738430398@qq.com>
2024-03-04 15:56:59 +0800

    Delete src directory

--- src/main/java/com/gohoy/mapper/TransactionDataMapper.java
+++
@@ -1,9 +0,0 @@
-package com.gohoy.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.gohoy.entities.TransactionData;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionDataMapper extends BaseMapper<TransactionData> {
-}
